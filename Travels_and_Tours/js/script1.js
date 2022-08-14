6d42b7caf192e777739ea20a7e1c247788b7dc3b
Abhishek <63811069+abhishek23022002@users.noreply.github.com>
2022-08-14 10:19:08 +0530

    Add files via upload

---
+++ Travels_and_Tours/js/script1.js
@@ -0,0 +1,11 @@
+<script>
+if (annyang)
+// Let's define a command.
+var commands ={
+'hello': function(){ alert('Hello world!'); }
+//Add our commands to annyang
+annyang.addCommands(commands);
+// Start listening.
+annyang.start();
+}
+</script>
